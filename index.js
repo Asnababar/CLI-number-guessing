b41f96be96be62ddccd6d82de20eb384d18bcfb7
Asnababar <asna.babar19@gmail.com>
2024-03-30 12:20:09 +0500

    work complete

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+//SHABANG
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+const answers = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "Guess the number between 1 to 10"
+    }
+]);
+if (answers.userGuess === randomNumber) {
+    console.log("You guessed it right");
+}
+else {
+    console.log("You guessed it wrong");
+}
